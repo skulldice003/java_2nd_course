e9ce337aeb4502650f6e08d397ced74435436f7d
NEO <39527922+skulldice003@users.noreply.github.com>
2024-09-18 20:12:43 +0700

    update  Eureka server

---
+++ ServerRun/src/main/java/com/udacity/serverrun/ServerRunApplication.java
@@ -0,0 +1,15 @@
+package com.udacity.serverrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerRunApplication.class, args);
+    }
+
+}
